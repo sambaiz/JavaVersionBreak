4af58a5af5e357f008f5416d0f396f0ceb845441
saisa6153 <yuki.tannai@gmail.com>
2013-06-01 16:55:37 +0900

    Androidアプリ作成

---
+++ JavaVersionBreak/src/Javersion.java
@@ -0,0 +1,28 @@
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Javersion {
+	private static final String VERSION_STRING = "^JDK([1-9]?[0-9]+)u([1-9]?[0-9]+)$";
+	private static final Pattern VERSION_PATTERN = Pattern.compile(VERSION_STRING);
+
+	public static boolean isValid(String version) {
+		if(version == null) throw new IllegalArgumentException();
+		Matcher matcher = VERSION_PATTERN.matcher(version);
+		if(matcher.find() && Integer.parseInt(matcher.group(1)) >= 7) return true;
+		return false;
+	}
+
+	public static VersionNumber parse(String version) throws ParseException {
+		try{
+			if (! isValid(version)) throw new ParseException(version, 0);
+		}catch(IllegalArgumentException e){
+			throw new ParseException(version, 0);
+		}
+		Matcher matcher = VERSION_PATTERN.matcher(version);
+		matcher.find();
+		return new VersionNumber(Integer.parseInt(matcher.group(1)) ,Integer.parseInt(matcher.group(2)));
+	}
+
+}
---
+++ JavaVersionBreak/test/JaversionTest.java
@@ -0,0 +1,86 @@
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+
+import org.junit.Test;
+
+
+public class JaversionTest {
+
+	@Test
+	public void JDK_7以上の整数_u_0以上の整数_のときisValidがtrueを返すこと() {
+		assertTrue(Javersion.isValid("JDK7u0"));
+		assertFalse(Javersion.isValid("JDK6u6"));
+		assertFalse(Javersion.isValid("hogehoge"));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void _isValidはNullを渡された時例外を投げること(){
+		Javersion.isValid(null);
+	}
+
+	@Test
+	public void JDK_Nu_MのときfamilyNumberがNupdateNumberがMのオブジェクトを返すこと() throws ParseException{
+		assertEquals(7, Javersion.parse("JDK7u0").getFamilyNumber());
+		assertEquals(0, Javersion.parse("JDK7u0").getUpdateNumber());
+		assertEquals(8, Javersion.parse("JDK8u0").getFamilyNumber());
+	}
+
+	@Test(expected=ParseException.class)
+	public void isValidがtrueでないときparseは例外を投げること() throws ParseException{
+		Javersion.parse("hogehoge");
+		Javersion.parse(null); //IllegalArgumentException
+	}
+
+	@Test
+	public void changeToLongは下位32bitにunを振り上位32bitはfnを振ったものを返すこと() throws ParseException{
+		assertEquals((7L << 32L) + 1L ,VersionNumber.changeToLong(Javersion.parse("JDK7u1")));
+	}
+
+	@Test
+	public void gtは引数とバージョンを比較し引数よりバージョンが大きければtrueを返すこと() throws ParseException{
+		assertTrue(Javersion.parse("JDK7u1").gt(Javersion.parse("JDK7u0")));
+		assertTrue(Javersion.parse("JDK8u1").gt(Javersion.parse("JDK7u1")));
+		assertFalse(Javersion.parse("JDK7u1").gt(Javersion.parse("JDK7u3")));
+		assertFalse(Javersion.parse("JDK7u1").gt(Javersion.parse("JDK7u1")));
+	}
+
+	@Test
+	public void ltは引数とバージョンを比較し引数よりバージョンが小さければtrueを返すこと() throws ParseException{
+		assertTrue(Javersion.parse("JDK7u0").lt(Javersion.parse("JDK7u1")));
+		assertTrue(Javersion.parse("JDK7u1").lt(Javersion.parse("JDK8u1")));
+		assertFalse(Javersion.parse("JDK7u3").lt(Javersion.parse("JDK7u1")));
+		assertFalse(Javersion.parse("JDK7u1").lt(Javersion.parse("JDK7u1")));
+	}
+
+	@Test
+	public void nextLimitedUpdateはLUを適用したバージョンのオブジェクトを返すこと() throws ParseException{
+		assertEquals(7, Javersion.parse("JDK7u0").nextLimitedUpdate().getFamilyNumber());
+		assertEquals(20, Javersion.parse("JDK7u0").nextLimitedUpdate().getUpdateNumber());
+	}
+
+	@Test
+	public void nextCriticalPatchUpdateはCPUを適用したバージョンのオブジェクトを返すこと() throws ParseException{
+		assertEquals(7, Javersion.parse("JDK7u0").nextCriticalPatchUpdate().getFamilyNumber());
+		assertEquals(5, Javersion.parse("JDK7u0").nextCriticalPatchUpdate().getUpdateNumber());
+		assertEquals(11, Javersion.parse("JDK7u9").nextCriticalPatchUpdate().getUpdateNumber());
+	}
+	
+	@Test
+	public void nextSecurityAlertは通常のアップデートを適用したバージョンのオブジェクトを返すこと() throws ParseException{
+		assertEquals(7, Javersion.parse("JDK7u0").nextSecurityUpdate().getFamilyNumber());
+		assertEquals(1, Javersion.parse("JDK7u0").nextSecurityUpdate().getUpdateNumber());
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void nextSecurityAlertが適用されたバージョンがLU_CPUと被るときは例外を投げること() throws ParseException{
+		Javersion.parse("JDK7u19").nextSecurityUpdate().getUpdateNumber();
+		Javersion.parse("JDK7u14").nextSecurityUpdate().getUpdateNumber();
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void nextCriticalPatchUpdateがLU_plus_1を使うときは例外を投げること() throws ParseException{
+		// Oracleから説明されていないケースは例外を投げさせる
+		Javersion.parse("JDK7u15").nextCriticalPatchUpdate().getUpdateNumber();
+	}
+}
