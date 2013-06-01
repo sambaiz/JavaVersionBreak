06433e030aaaad69ead8c384c1c52817877d34d6
saisa6153 <yuki.tannai@gmail.com>
2013-06-01 13:54:34 +0900

    isValid()がfalseのときparse()は例外を投げること

---
+++ src/VersionNumber.java
@@ -0,0 +1,13 @@
+
+public class VersionNumber {
+
+	public int familyNumber;
+	public int updateNumber;
+	
+	public VersionNumber(int familyNumber, int updateNumber) {
+		super();
+		this.familyNumber = familyNumber;
+		this.updateNumber = updateNumber;
+	}
+
+}
--- test/JaversionTest.java
+++ test/JaversionTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+
 import org.junit.Test;
 
 
@@ -8,9 +10,26 @@ public class JaversionTest {
 	@Test
 	public void JDK_7以上の整数_u_0以上の整数_のときisValidがtrueを返すこと() {
 		assertTrue(Javersion.isValid("JDK7u0"));
-		assertTrue(Javersion.isValid("JDK8u1"));
 		assertFalse(Javersion.isValid("JDK6u6"));
 		assertFalse(Javersion.isValid("hogehoge"));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void _isValidはNullを渡された時例外を投げること(){
+		Javersion.isValid(null);
+	}
+	
+	@Test
+	public void JDK_Nu_MのときfamilyNumberがNupdateNumberがMのオブジェクトを返すこと() throws ParseException{
+		assertEquals(7, Javersion.parse("JDK7u0").familyNumber);
+		assertEquals(0, Javersion.parse("JDK7u0").updateNumber);
+		assertEquals(8, Javersion.parse("JDK8u0").familyNumber);
+	}
+
+	@Test(expected=ParseException.class)
+	public void isValidがfalseのときparseは例外を投げること() throws ParseException{
+		Javersion.parse("hogehoge");
+		Javersion.parse(null);
 	}
 
 }
